e61c82487d60c45fc03a9ef73fb8d95b94b9aa0b
Quanost <nguyenquanost3@gmail.com~>
2022-10-31 10:16:52 +0700

    Finish config API gateway

---
+++ ThucHanhTuan8/src/main/java/com/example/demo/ThucHanhTuan8Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThucHanhTuan8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThucHanhTuan8Application.class, args);
+	}
+
+}
